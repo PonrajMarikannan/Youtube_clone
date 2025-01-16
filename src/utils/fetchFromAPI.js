ca8379941b5d66e32ae4905581c2cded8bd010ec
PonrajMarikannan <ponrajmarikannan@gmail.com>
2025-01-16 13:04:32 +0530

    First Commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "049d22ef15mshe2036e06431522fp13b08ejsn536c052c99e6",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
